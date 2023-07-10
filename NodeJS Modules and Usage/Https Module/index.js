0476f4b71b58b86e4f2dae9fffb00fa1cd8d33b7
HummaX <hummas_ch@hotmail.com>
2023-07-11 00:21:35 +0500

    writehead

--- NodeJS Modules and Usage/Https Module/index.js
+++ NodeJS Modules and Usage/Https Module/index.js
@@ -8,3 +8,7 @@ let server = http.createServer((req,res)=>{
 server.listen(3000,'127.0.0.1',()=>{ // we can specify local host address too but cannot change it to other address only to home
     console.log('listening on port 3000')
 })
+
+// Commands
+res.end // To display text on target page/url
+res.writeHead // to send status code and send headers
