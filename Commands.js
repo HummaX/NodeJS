15553726338c0c721b4a7fe90b3fd48b167e7d90
HummaX <hummas_ch@hotmail.com>
2023-07-18 00:43:43 +0500

    updated commands

--- Commands.js
+++ Commands.js
@@ -9,3 +9,9 @@ npm install {package name} --save-dev
 
 // save dependencies as global
 npm i nodemon --global
+
+// installing packe with specific version
+npm install slugify@1.0.0
+
+// To check outdated packages
+npm outdated
