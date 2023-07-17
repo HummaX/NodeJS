605a736708ac47beffa3944e82d578d3362b6ba2
HummaX <hummas_ch@hotmail.com>
2023-07-17 22:24:47 +0500

    updated commands

--- Commands.js
+++ Commands.js
@@ -1,3 +1,9 @@
+// to auto start app
+"scripts":{
+    "start":"nodemon index.js"
+}
+npm start // to run this file
+
 // To save dependency as DevDependencies
 npm install {package name} --save-dev
 
