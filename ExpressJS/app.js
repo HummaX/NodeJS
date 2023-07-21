ba371ebabefc03f1b4a8a034b38546813db7eecf
HummaX <hummas_ch@hotmail.com>
2023-07-21 20:00:57 +0500

    Update app.js

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -3,11 +3,12 @@ let express = require('express')
 let app = express()
 
 // parse because data is in json we have to chnage it to object
+// we are using readfilesync bcz it will first read whole file then the other code on server, 
+//as if file is big and we use reafile it will carsh ths server // VIDEO 52
 let tours = JSON.parse(fs.readFileSync('./api-data/tours-simple.json'))
-console.log(tours)
 
 app.get('/',(req,res)=>{
-    res.status(200).json({tours:tours})
+    res.status(200).json({status:"Success",tours:tours})
 })
 
 app.listen(3000)
