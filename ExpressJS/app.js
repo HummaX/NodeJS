7467f3dcc7e1449438532d2618fb9ca13ace8c9a
HummaX <hummas_ch@hotmail.com>
2023-07-26 19:54:44 +0500

    Update app.js

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -3,25 +3,27 @@ let express = require('express')
 let morgan = require('morgan')
 let app = express()
 
-
+// Middlewares
 app.use(express.json())
 // parse because data is in json we have to chnage it to object
 // we are using readfilesync bcz it will first read whole file then the other code on server, 
 //as if file is big and we use reafile it will carsh ths server // VIDEO 52
 let tours = JSON.parse(fs.readFileSync('./api-data/tours-simple.json'))
 
-// app.get('/api/v1/tours',(req,res)=>{
-//     res.status(200).json(
-//         {
-//             status:"Success",
-//             results:tours.length,
-//             data:{tours:tours}
-//         }
-//         )
-// })
+
+// Routes Handlers
+let getAllTours = app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json(
+        {
+            status:"Success",
+            results:tours.length,
+            data:{tours:tours}
+        }
+        )
+})
 
 
-let getAllTours = async(req,res)=>{
+let toursPostRequest = async(req,res)=>{
     let newId = tours[tours.length-1].id +1 
     let toursId = Object.assign({id:newId},req.body)
     console.log(toursId)
@@ -45,14 +47,14 @@ return tour.id == req.params.id
     console.log(tour)
 }
 
-app.post('/api/v1/tours',getAllTours)
+app.post('/api/v1/tours',toursPostRequest)
 app.delete('/api/v1/tours/:id',deleteTour)
 // Params
 app.get('/api/v1/tours/:id',getTourById)
 
 // methods for same router
-app.route('/api/v1/tours').get(getAllTours)
-app.route('/api/v1/tours/:id').delete(deleteTour).get(getTourById)
+app.route('/api/v1/tours').post(toursPostRequest)
+app.route('/api/v1/tours/:id').delete(deleteTour).get(getTourById,getAllTours)
 
 
 app.listen(3000)
