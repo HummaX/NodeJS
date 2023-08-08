4bc630bd16f07c1082eecbc5357416ab3d4ff339
HummaX <hummas_ch@hotmail.com>
2023-08-08 20:53:25 +0500

    validator

--- Mongoose/Scheme & Model/Schema Options.js
+++ Mongoose/Scheme & Model/Schema Options.js
@@ -4,6 +4,7 @@ let schemaClass = new mongoose.Schema({
     name: {
         type: String, 
         required:[true,'A data is requires here must'],// Either true or false ,['This is Err we want to give']
+        // Required is also called validator
         default: 'John Doe', // if no value is given
         unique: true, // if its true and there must be no other entry similar to this, this case applies in email
         index: true,
