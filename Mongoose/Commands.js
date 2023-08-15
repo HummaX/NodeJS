e75f742de6232eb630298b28d0e1173a27222834
HummaX <hummas_ch@hotmail.com>
2023-08-15 20:34:10 +0500

    Update Commands.js

--- Mongoose/Commands.js
+++ Mongoose/Commands.js
@@ -1,9 +1,10 @@
 const { model } = require("mongoose")
 
 // To save model
+let newToour = newTour()
 await model.save()
 
 // To avoid new tour and saving we coud use this instead
 //So, if you want to create a new instance of a model and save it to the database in one step, you can use the create method. 
 //If you have an existing instance of a model that you want to save to the database, you should use the save method
-await model.create(req.body)
+let newTour = await model.create(req.body)
