7937760cc16bf680a554de9ab9abf689ef6be93c
HummaX <hummas_ch@hotmail.com>
2023-08-15 23:07:39 +0500

    Update app.js

---
+++ Mongoose/Pratice/app.js
@@ -0,0 +1,19 @@
+let mongoose = require('mongoose')
+
+let tourSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'A Tour Must Have a name'],
+        unique:true
+    },
+    rating:{
+type:Number,
+default:4.5
+    },
+    price:{
+        type:Number,
+        required:[true,'a Tour Must Have a Price']
+    }
+})
+
+
