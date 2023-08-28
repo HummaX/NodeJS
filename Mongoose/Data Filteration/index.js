8abd8dc9765fb41a6bf74311de7724acc6107544
HummaX <hummas_ch@hotmail.com>
2023-08-29 00:35:28 +0500

    Update index.js

--- Mongoose/Data Filteration/index.js
+++ Mongoose/Data Filteration/index.js
@@ -10,6 +10,7 @@ exports.getallTours = async(req,res)=>{
 
 // to use mongoDB operators in mongoose (video 96)
 let testingTour = async(req,res)=>{
+    // URL http://localhost:3000/api/v1/testing?price[gte]=2900&duration[lte]=10
     let queryString = JSON.stringify(req.query) // since data is not in string we cannot use replace
     queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`) // regex to use
     // this will match these opearators | represnts or operator in this regex, and replace takes 2nd argument as function/value
