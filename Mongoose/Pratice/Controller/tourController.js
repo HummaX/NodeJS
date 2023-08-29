6149ad105df49b091522ac72a746fee7d9d2787a
HummaX <hummas_ch@hotmail.com>
2023-08-30 00:29:52 +0500

    sorting

--- Mongoose/Pratice/Controller/tourController.js
+++ Mongoose/Pratice/Controller/tourController.js
@@ -28,10 +28,16 @@ let tourFiltering = async(req,res)=>{
 
 let testingTour = async(req,res)=>{
     console.log(req.query)
+    req.query.sort = req.query.sort.split(',').join(' ')
     let data = await Tour.find().sort(req.query.sort) // ascending order (lower to higher)
     // url http://localhost:3000/api/v1/testing?sort=price
 
     // for decending (higher to lower) order put - in price in url of postman eg: http://localhost:3000/api/v1/testing?sort=-price
+
+    // for one or more sorting
+    http://localhost:3000/api/v1/testing?sort=-price,-ratingsQuantity
+    req.query.sort = req.query.sort.split(',').join(' ')
+    let data = await Tour.find().sort(req.query.sort) // decending order (lower to higher) for acendinf remove -
 
     res.status(200).json({data:data})
 }
