c06cca28d889fa2afc6752bc67c52602e214eefa
HummaX <hummas_ch@hotmail.com>
2023-11-24 05:26:44 +0500

    middleware

---
+++ Mongoose/Middleware/index.js
@@ -0,0 +1,7 @@
+// There are 2 type of middleware, and middleware can work with Document, Query, Aggregate, Model
+// 1: Runs before Document Processing/Working
+// 2: Runs after Document Processing/Working
+
+// Middleware before
+
+// Middleware After
