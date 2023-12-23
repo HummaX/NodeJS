983a5ddef1e7c7d9eff1e3ecc5e3254b6c54faad
HummaX <hummas_ch@hotmail.com>
2023-12-23 07:32:37 +0500

    Create index.js

---
+++ JWT Practice/Hashing Password/index.js
@@ -0,0 +1,31 @@
+let mongoose = require("mongoose");
+// var validator = require('validator');
+const bcrypt = require('bcrypt');
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  email: {
+      type: String,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type:String,
+    required: true,
+    minLength: 8,
+  },
+  confirmPassword:{
+    type:String,
+    required:[true, 'Password is required'],
+    validate:{validator:function(value){return value === this.password},message:'Password are not same'}
+  }
+});
+
+userSchema.pre('save',async function(next){
+  if(!this.isModified('password')) return console.log('modified')
+
+  this.password = await bcrypt.hash(this.password, 10) // returns promiss
+  this.confirmPassword = null // mongoose works only till validation after validation while sending data to DB we can change data
+
+return next()
+})
